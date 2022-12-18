e9de7585a88ddc7ff4edc17bf0d87bfdbf411902
AlexxBelous <connectionwithme1@gmail.com>
2022-12-18 14:16:38 +0200

    a

--- main/script.js
+++ main/script.js
@@ -1,3 +1,17 @@
 // С помощью инструкции switch определите является ли число в переменной mount зимним месяцем.
 
 const mount = 2;
+
+switch (mount) {
+    case 12:
+        console.log('December')
+        break
+    case 1:
+        console.log('January')
+        break
+    case 2:
+        console.log('February')
+        break
+    default:
+        console.log('This month is not winter')
+}
