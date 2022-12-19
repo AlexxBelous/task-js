a3ed68fb4f42dd55b5a5074b591d08aff74b06eb
Alex <connectionwithme1@gmail.com>
2022-12-19 11:07:56 +0200

    a

--- main/script.js
+++ main/script.js
@@ -1,3 +1,10 @@
 // Необходимо повесить на кнопки обработчик событий. При клике должен выводиться текст click!
 
 
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach(button => {
+  button.addEventListener('click', function() {
+    console.log('click!')
+  })
+})
