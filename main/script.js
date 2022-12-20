966df99dc58432fef7cbaeb7bfae4644cf534f3d
AlexxBelous <connectionwithme1@gmail.com>
2022-12-20 07:40:31 +0200

    a

--- main/script.js
+++ main/script.js
@@ -9,8 +9,26 @@
  * по собственным свойствам объекта
  */
 
-
-// Код писать здесь
-
-
+const objectWithNumbers = {
+    a: 10,
+    b: 20,
+    c: 'string',
+    d: 12,
+  }
+  
+  function sumObjectValues(nums) {
+    let sum = 0
+  
+    Object.keys(nums).forEach((key) => {
+      if (typeof nums[key] === 'number') {
+        sum += nums[key]
+      }
+    })
+  
+    return sum
+  }
+  
+  const result = sumObjectValues(objectWithNumbers)
+  console.log(result)
+  
   //42
